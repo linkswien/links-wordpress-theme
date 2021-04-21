829c0f7da4748ba870cf27964450067a2b6a4c8f
Bernhard Hayden <hello@nini.su>
2021-04-21 17:27:02 +0200

    Refactor header image code

--- single.php
+++ single.php
@@ -16,11 +16,12 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			if ( has_post_thumbnail( $post->ID ) ) {
-				$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-				echo '<div class="wp-block-cover has-background-dim-20 has-background-dim is-position-center-center taller" style="background-image:url(\'' . $image . '\')"></div>';
+				$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];
 			} else {
-				echo get_bezirksseiten_header(get_the_category());
+				$image = get_bezirksseiten_header_image(get_the_category());
 			}
+			echo '<div class="wp-block-cover has-background-dim-20 has-background-dim is-position-center-center taller" style="background-image:url(\'' . $image . '\')"></div>';
+
 			the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );
