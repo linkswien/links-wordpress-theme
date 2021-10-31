c54415743bf7add5b71dc48c541714521c1123fb
Bernhard Hayden <hello@nini.su>
2021-10-31 14:04:22 +0100

    Wildes Website Wochenende Merge Part 1 (#27)
    * fix hover on social media icons in footer
    
    * fix bug in insert_og_in_head when it is loaded on a non-post page
    
    * add screenshot.png
    
    * Create menschen_von_links.php
    
    * Update menschen_von_links.php
    
    * Implement Menschen von Links single post template
    
    * Update single.php
    
    * Update menschen-von-links.php
    
    * remove unused full-width-purple widget code
    
    * Update single.php
    
    * Update single.php
    
    * Include menschen-von-links.php in functions.php
    
    * Update menschen-von-links.php
    
    * Add Menschen von Links Layout
    
    * Update content-menschen-von-links.php
    
    * Update content-menschen-von-links.php
    
    * Restyle Mitmachen form (#26)
    
    * Add footer HTML and styles for new mitmachen form
    
    * Fix form field background styles
    
    * More style fixes
    
    * Fix textarea issues
    
    * More style fixes
    
    * Fix wrapping issues
    
    * Fix form styling bugs
    
    * Remove Mitmachen footer from widgets
    
    * Minor form fixes
    
    * Add Mautic script
    
    * Update content-menschen-von-links.php
    
    * Update content-menschen-von-links.php
    
    * Update content-menschen-von-links.php
    
    * Add Menschen von LINKS photo caption rule to style.scss
    
    * Style form buttons and messages
    
    * Update style.scss
    
    * Change Mitmachen form to hopefully fix error bug
    
    * Final form fix and now let's move on…
    
    * Update single.php
    
    Co-authored-by: Konrad Pozniak <k.pozniak@gmx.at>
    Co-authored-by: flow754 <rath754@gmail.com>

--- functions.php
+++ functions.php
@@ -129,15 +129,6 @@ add_action( 'after_setup_theme', 'links_wien_theme_content_width', 0 );
  */
 function links_wien_theme_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Footer 1', 'links-wien-theme' ),
-		'id'            => 'footer-1',
-		'description'   => esc_html__( 'Add widgets here.', 'links-wien-theme' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-	register_sidebar( array(
 		'name'          => esc_html__( 'Footer 2', 'links-wien-theme' ),
 		'id'            => 'footer-2',
 		'description'   => esc_html__( 'Add widgets here.', 'links-wien-theme' ),
@@ -159,7 +150,7 @@ function links_wien_theme_scripts() {
 
 	wp_enqueue_script( 'links-wien-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'links-wien-theme-bezirk-helper', get_template_directory_uri() . '/js/bezirk-helper.js', array(), '20151215', true );
+	wp_enqueue_script( 'links-wien-theme-helpers', get_template_directory_uri() . '/js/helpers.js', array(), '20151215', true );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -184,7 +175,7 @@ function insert_og_in_head() {
 	echo '<meta property="og:site_name" content="' . get_bloginfo('name') . '"/>';
 
 	// Try getting post or category header picture
-	if ( has_post_thumbnail( $post->ID ) ) {
+	if ( $post && has_post_thumbnail( $post->ID ) ) {
 		$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];
 	} else {
 		$image = get_bezirksseiten_header_image(get_the_category());
@@ -220,8 +211,8 @@ function insert_og_in_head() {
 	}
 	echo '<meta name="description" content="' . $excerpt . '" />';
 	echo '<meta property="og:description" content="' . $excerpt . '" />';
-  echo '<meta property="og:title" content="' . get_the_title() . ' – ' . get_bloginfo('name') . '"/>';
-  echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+    echo '<meta property="og:title" content="' . get_the_title() . ' – ' . get_bloginfo('name') . '"/>';
+    echo '<meta property="og:url" content="' . get_permalink() . '"/>';
 }
 add_action( 'wp_head', 'insert_og_in_head', 5 );
 
@@ -256,6 +247,11 @@ if ( defined( 'JETPACK__VERSION' ) ) {
  * Load function to display bezirkssite header
  */
 require __DIR__ . '/inc/bezirkssite-header.php';
+
+/**
+ * Load function to display 'Menschen von Links' single posts
+ */
+require __DIR__ . '/inc/menschen-von-links.php';
 
 /**
  * Activate shortcuts in footers.
