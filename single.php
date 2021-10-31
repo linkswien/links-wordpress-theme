8c7b16c4e8091ee5be4900a363b963d2428a319b
flow754 <rath754@gmail.com>
2021-10-31 13:06:06 +0100

    Update single.php

--- single.php
+++ single.php
@@ -40,7 +40,9 @@ get_header();
 				get_template_part( 'template-parts/content', get_post_type() );
 			}
 
-			the_post_navigation();
+			if ( !$is_menschen_von_links ) {
+				the_post_navigation();
+			}
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
